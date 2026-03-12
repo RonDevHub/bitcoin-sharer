bf700a4608be3d43e79c361d4e629396caf8e4c0
RonDevHub <no_reply@rondev.de>
2026-03-12 15:31:31 +0000

    src/app/Renderer.php aktualisiert

--- src/app/Renderer.php
+++ src/app/Renderer.php
@@ -2,7 +2,7 @@
 namespace App;
 
 class Renderer {
-    public static function render(string $viewData, bool $error, ?string $generatedLink, I18n $i18n, string $qrOutput = ""): void {
+    public static function render(string $viewData, bool $error, ?string $generatedLink, I18n $i18n, string $qrOutput = "", bool $validationError = false): void {
         $lang = $i18n->getLang();
         include __DIR__ . '/../public/template.php';
     }
